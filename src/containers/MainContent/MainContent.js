d170d198a0f6ed9da87a145bd4e7718671c056b0
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-08-23 13:48:12 +1200

    add book detail bar

--- src/containers/MainContent/MainContent.js
+++ src/containers/MainContent/MainContent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from './MainContent.module.css';
 import NavCardBar from '../../components/NavCardBar/NavCardBar';
+import BookDetailBar from '../../components/BookDetailBar/BookDetailBar';
 
 const MainContent = (props) => {
     return (
         <div className={classes.MainContent}>
             <NavCardBar />
+            <BookDetailBar />
         </div>
     );
 };
