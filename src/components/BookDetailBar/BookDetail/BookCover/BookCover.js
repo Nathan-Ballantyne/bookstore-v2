ba749b0589213c9bc168ae57dfa85c993b6c4a16
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-08-23 13:49:04 +1200

    add book cover component

---
+++ src/components/BookDetailBar/BookDetail/BookCover/BookCover.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './BookCover.module.css';
+
+const BookCover = () => {
+    return (
+        <div className={classes.BookCover}>
+            Cover
+        </div>
+    );
+};
+
+export default BookCover;
