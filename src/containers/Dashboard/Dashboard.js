e41f058edcf99fa271be0195faabb4de21a1bceb
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-08-25 18:58:20 +1200

    revert to old nav bar

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Toolbar from '../../components/Toolbar/Toolbar';
-import SideDrawer from '../../components/Toolbar/SideDrawer/SideDrawer';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import MainContent from '../MainContent/MainContent';
 
 const Dashboard = () => {
     return (
         <>
-            <SideDrawer />
+            <Toolbar />
             <SearchBar />
             <MainContent />
         </>
