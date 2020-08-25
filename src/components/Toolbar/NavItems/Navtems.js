3275bc2704589ba25d842211bae56bac54a27623
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-08-25 19:10:11 +1200

    add setting nav item

--- src/components/Toolbar/NavItems/Navtems.js
+++ src/components/Toolbar/NavItems/Navtems.js
@@ -5,12 +5,14 @@ import bookIcon from '../../../assets/Icons/book-icon.png';
 import listIcon from '../../../assets/Icons/list-icon.png';
 import statsIcon from '../../../assets/Icons/stats-icon.png';
 import userIcon from '../../../assets/Icons/user-icon.png';
+import settingIcon from '../../../assets/Icons/setting-icon.png';
 
 const navItems = [
     { type: 'main', icon: bookIcon, title: 'Bookstore', alt: 'main-icon' },
     { type: 'detail', icon: userIcon, title: 'Profile', alt: 'profile-icon' },
     { type: 'detail', icon: listIcon, title: 'Lists', alt: 'lists-icon' },
     { type: 'detail', icon: statsIcon, title: 'Stats', alt: 'stats-icon' },
+    { type: 'detail', icon: settingIcon, title: 'Settings', alt: 'setting-icon' },
 ];
 
 const NavigationItems = () => {
