0f438fe58a5a07e05f1d57fe8d6d9fce7bcf3f1a
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-08-26 19:43:31 +1200

    use burger menu and menu components for tool bar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ColapsedNavBar from './ColapsedNavBar/ColapsedNavBar';
-import NavBar from './NavBar/NavBar';
+import React, { useState } from 'react';
+import BurgerMenu from './BurgerMenu/BurgerMenu';
+import Menu from './Menu/Menu';
+
 
 const Toolbar = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <>
-            <ColapsedNavBar />
-            <NavBar />
-        </>
+        <div>
+
+            <BurgerMenu open={open} setOpen={setOpen} />
+            <Menu open={open} setOpen={setOpen} />
+        </div>
     );
 };
 
