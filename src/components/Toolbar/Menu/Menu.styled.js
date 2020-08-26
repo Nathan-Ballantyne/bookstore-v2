1f33d845444ced9fed5f1d2d870493fb59443b92
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-08-26 20:00:37 +1200

    remove unneeded styling

--- src/components/Toolbar/Menu/Menu.styled.js
+++ src/components/Toolbar/Menu/Menu.styled.js
@@ -1,40 +1,16 @@
 import styled from 'styled-components';
 
 export const StyledMenu = styled.nav`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  background: #a3d2ca;
-  height: 100vh;
-  text-align: left;
-  padding: 2rem;
-  position: absolute;
-  top: 0;
-  left: 0;
-  transition: transform 0.3s ease-in-out;
-  transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
-  
-  @media (max-width: 576px) {
-    width: 100%;
-  }
-
-  a {
-    font-size: 2rem;
-    text-transform: uppercase;
-    padding: 2rem 0;
-    font-weight: bold;
-    letter-spacing: 0.5rem;
-    color: #056676;
-    text-decoration: none;
-    transition: color 0.3s linear;
-    
-    @media (max-width: 576px) {
-      font-size: 1.5rem;
-      text-align: center;
-    }
-
-    &:hover {
-      color: #a3d2ca;
-    }
-  }
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background: #a3d2ca;
+    height: 100vh;
+    text-align: left;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transition: transform 0.3s ease-in-out;
+    transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(-100%)')};
+    z-index: 90;
 `;
