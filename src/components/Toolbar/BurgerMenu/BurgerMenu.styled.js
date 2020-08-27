7bd570ef7844e0832b90f5d98720709577834fd3
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-08-27 19:05:33 +1200

    move burger menu to be lined up with book icon

--- src/components/Toolbar/BurgerMenu/BurgerMenu.styled.js
+++ src/components/Toolbar/BurgerMenu/BurgerMenu.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledBurger = styled.button`
   position: absolute;
   top: 5%;
-  left: 2rem;
+  left: 1.6rem;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
