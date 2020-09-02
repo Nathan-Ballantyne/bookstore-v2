4156eae43a8e98ae51a3bc970ba0db82513071d8
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-09-02 20:03:03 +1200

    add bookstore title component

---
+++ src/components/UI/Title/Title.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const TitleStyle = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@1,500&display=swap');
+    font-family: 'Lora', serif;
+    font-size: 100px;
+    color: #056676;
+    margin: auto;
+    width: 50%;
+    text-align: center;
+`;
