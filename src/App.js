4784a246bb2706e47bfd23c78901bc934eb8697f
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-09-02 20:15:09 +1200

    add react routing and route paths

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Dashboard from './containers/Dashboard/Dashboard';
+import LoginPage from './containers/Login/Login';
 
 function App() {
-    return <Dashboard />;
+    return (
+        <Switch>
+            <Route path='/login' component={LoginPage} />
+            <Route path='/' component={Dashboard} />
+        </Switch>
+    );
 }
 
 export default App;
