06cabf1f71cc6415259e84ad59daac2fb293e424
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-09-02 20:05:00 +1200

    remove login button

--- src/components/Toolbar/Menu/Menu.js
+++ src/components/Toolbar/Menu/Menu.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { bool } from 'prop-types';
 import { StyledMenu } from './Menu.styled';
 import NavItems from '../NavItems/Navtems';
-import GoogleLoginButton from '../../GoogleAuth/LoginHooks/LoginHooks';
 import GoogleLogOutButton from '../../GoogleAuth/LogoutHooks/LogoutHooks';
 
 const Menu = ({ open }) => {
   return (
     <StyledMenu open={open}>
       <NavItems />
-      <GoogleLoginButton />
       <GoogleLogOutButton />
     </StyledMenu>
   )
