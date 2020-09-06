f74f4ff42889199c9e3d40a73aa5399e4df3daa7
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-09-06 13:21:01 +1200

    add loading spinner

--- src/components/BookDetailBar/BookDetailBar.js
+++ src/components/BookDetailBar/BookDetailBar.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState, memo } from 'react';
 import BookDetail from './BookDetail/BookDetail';
+import Spinner from '../UI/Spinner/Spinner';
 import classes from './BookDetailBar.module.css';
 import axios from 'axios';
 
 const BookDetailBar = (props) => {
     const [books, setBooks] = useState([]);
     const [shelfId, setShelfId] = useState(0);
+    const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
         console.log(props.shelfId);
-        
+
         if (books.length === 0 || shelfId !== props.shelfId) {
+            setLoaded(false);
             axios
                 .get(
                     `https://www.googleapis.com/books/v1/users/101691569536719382664/bookshelves/${props.shelfId}/volumes`
@@ -18,28 +21,31 @@ const BookDetailBar = (props) => {
                 .then((response) => {
                     setBooks(response.data.items);
                     setShelfId(props.shelfId);
+                    setLoaded(true);
                 })
                 .catch(console.log);
-            }
-    });
+        }
+    }, [props.shelfId, books.length, shelfId]);
 
-    return (
-        <div className={classes.BookDetailBar}>
-            {books.map((book) => {
-                return (
-                    <BookDetail
-                        key={book.id}
-                        id={book.id}
-                        // rating={book.rating}
-                        title={book.volumeInfo.title}
-                        author={book.volumeInfo.authors.join(', ')}
-                        // getBookDetails={props.getBookDetails}
-                        img={book.volumeInfo.imageLinks.smallThumbnail}
-                    />
-                );
-            })}
-        </div>
-    );
+    let bookDetail = <Spinner />;
+
+    if (loaded) {
+        bookDetail = books.map((book) => {
+            return (
+                <BookDetail
+                    key={book.id}
+                    id={book.id}
+                    // rating={book.rating}
+                    title={book.volumeInfo.title}
+                    author={book.volumeInfo.authors.join(', ')}
+                    // getBookDetails={props.getBookDetails}
+                    img={book.volumeInfo.imageLinks.smallThumbnail}
+                />
+            );
+        });
+    }
+
+    return <div className={classes.BookDetailBar}>{bookDetail}</div>;
 };
 
 export default memo(BookDetailBar);
--- src/containers/MainContent/MainContent.js
+++ src/containers/MainContent/MainContent.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react';
 import classes from './MainContent.module.css';
 import BookshelfBar from '../../components/BookshelfBar/BookshelfBar';
 import BookDetailBar from '../../components/BookDetailBar/BookDetailBar';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from 'axios';
 
 const MainContent = () => {
     const [bookShelves, setBookShelves] = useState([]);
     const [shelfId, setShelfId] = useState(0);
+    const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
         if (bookShelves.length === 0) {
@@ -14,18 +16,27 @@ const MainContent = () => {
                 .get(
                     'https://www.googleapis.com/books/v1/users/101691569536719382664/bookshelves'
                 )
-                .then((response) => setBookShelves(response.data.items))
-                .catch('Error: ', console.log);
+                .then((response) => {
+                    setBookShelves(response.data.items);
+                    setLoaded(true);
+                })
+                .catch(console.log);
         }
     });
 
     const getShelf = (id) => {
         setShelfId(id);
+    };
+
+    let bookshelfBar = <Spinner />;
+    
+    if(loaded) {
+        bookshelfBar = <BookshelfBar changeShelf={getShelf} bookshelves={bookShelves} />;
     }
 
     return (
         <div className={classes.MainContent}>
-            <BookshelfBar changeShelf={getShelf} bookshelves={bookShelves} />
+            {bookshelfBar}
             <BookDetailBar shelfId={shelfId} />
         </div>
     );
