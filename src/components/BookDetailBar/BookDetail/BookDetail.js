56a0607097c0da5328f8ac71d961539213bd9085
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-09-07 19:53:15 +1200

    add details to modal

--- src/components/BookDetailBar/BookDetail/BookDetail.js
+++ src/components/BookDetailBar/BookDetail/BookDetail.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BookCover from './BookCover/BookCover';
 import Details from './Details/Details';
+import Modal from '../../UI/Modal/Modal';
 import classes from './BookDetail.module.css';
 
 const BookDetail = (props) => {
+    const [showModal, setShowModal] = useState(false);
+
+    const showDetails = () => {
+        setShowModal(true);
+    };
+
+    const modalClosed = () => {
+        setShowModal(false);
+    };
+
     return (
-        <div className={classes.BookDetail}>
-            <BookCover img={props.img} />
-            <Details
-                id={props.id}
-                rating={props.rating}
-                title={props.title}
-                author={props.author}
-                getBookDetails={props.getBookDetails}
-            />
-        </div>
+        <>
+            <Modal show={showModal} modalClosed={modalClosed}>
+                <p>{props.title}</p>
+                <p>{props.subtitle}</p>
+                <p>{props.description}</p>
+                <p><strong>Author: </strong>{props.author}</p>
+                <p><strong>Pages: </strong>{props.pagecount}</p>
+                <p><strong>Published: </strong>{props.publishedDate}</p>
+            </Modal>
+            <div className={classes.BookDetail}>
+                <BookCover img={props.img} />
+                <Details
+                    id={props.id}
+                    rating={props.rating}
+                    title={props.title}
+                    author={props.author}
+                    getBookDetails={showDetails}
+                />
+            </div>
+        </>
     );
 };
 
