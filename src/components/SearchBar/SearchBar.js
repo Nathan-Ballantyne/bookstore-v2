fd5ad4b73c675d691fc9baa5ea32d7e2a53cff3b
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-09-15 20:17:56 +1200

    create basic search function using google api and store results

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from './SearchBar.module.css';
+import axios from 'axios';
 
 const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    useEffect(() => {
+        if (searchTerm !== '') {
+            axios
+                .get(
+                    `https://www.googleapis.com/books/v1/volumes?q=${searchTerm.replace(
+                        ' ',
+                        '+'
+                    )}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`
+                )
+                .then((response) => {
+                    setSearchResults(response.data.items);
+                    console.log(response.data.items);
+                })
+                .catch(console.log);
+        }
+    }, [searchTerm, searchResults.length]);
+
     return (
         <input
             className={classes.SearchBar}
             placeholder={'Search books by name, genre or author'}
+            value={searchTerm}
+            onChange={handleChange}
         />
     );
 };
