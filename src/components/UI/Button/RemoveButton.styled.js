79cc314a09e0cd5d559b06fd57a09881a770b6e7
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-09-23 20:48:00 +1200

    add remove button

---
+++ src/components/UI/Button/RemoveButton.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const RemoveButton = styled.button`
+    @import url('https://fonts.googleapis.com/css2?family=Baloo+Tamma+2:wght@500&display=swap');
+    height: 20px;
+    width: 60px;
+    border-radius: 10px;
+    background-color: red;
+    font-family: 'Baloo Tamma 2', cursive;
+    font-size: 12px;
+    color: white;
+    border: none;
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+export default RemoveButton;
