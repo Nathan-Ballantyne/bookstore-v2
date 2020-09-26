548770bbe6ff9ef17dcf13dc97cae14705944ebd
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-09-26 18:07:55 +1200

    add small title style

--- src/components/UI/Title/Title.styled.js
+++ src/components/UI/Title/Title.styled.js
@@ -9,3 +9,14 @@ export const TitleStyle = styled.div`
     width: 50%;
     text-align: center;
 `;
+
+export const TitleStyleSmall = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@1,500&display=swap');
+    font-family: 'Lora', serif;
+    font-size: 40px;
+    color: #056676;
+    margin-top: 10px;
+    /* margin: auto; */
+    /* width: 50%; */
+    /* text-align: center; */
+`;
