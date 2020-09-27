bbb9504a5ba865a61f7f80f817196d372d97f7f8
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-09-27 14:36:24 +1300

    add color props

--- src/components/UI/Button/AddRemoveButton.styled.js
+++ src/components/UI/Button/AddRemoveButton.styled.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
-const RemoveButton = styled.button`
+const AddRemoveButton = styled.button`
     @import url('https://fonts.googleapis.com/css2?family=Baloo+Tamma+2:wght@500&display=swap');
     height: 20px;
     width: 60px;
     border-radius: 10px;
-    background-color: red;
+    background-color: ${props => props.color};
     font-family: 'Baloo Tamma 2', cursive;
     font-size: 12px;
     color: white;
@@ -15,4 +15,4 @@ const RemoveButton = styled.button`
     }
 `;
 
-export default RemoveButton;
+export default AddRemoveButton;
