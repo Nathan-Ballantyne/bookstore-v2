7bb349f3a79fd040238c3ca24436f677b64e8eb9
Nathan Ballantyne <nathanballantyne@gmail.com>
2020-10-03 14:16:04 +1300

    change styling

--- src/components/UI/Modal/SelectShelfModal.js
+++ src/components/UI/Modal/SelectShelfModal.js
@@ -1,6 +1,33 @@
 import React, { useState } from 'react';
 import Modal from './Modal';
 import AddRemoveButton from '../Button/AddRemoveButton.styled';
+import styled from 'styled-components';
+
+const Select = styled.select`
+    width: 100%;
+    height: 35px;
+    background: white;
+    color: gray;
+    padding-left: 5px;
+    font-size: 14px;
+    border-radius: 5px;
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+
+    &:focus {
+        outline: none;
+    }
+
+    option {
+        color: black;
+        background: white;
+        display: flex;
+        white-space: pre;
+        min-height: 20px;
+        padding: 0px 2px 1px;
+    }
+`;
 
 const SelectShelfModal = ({ options, addBook, show, setShow }) => {
     const [shelfId, setshelfId] = useState(0);
@@ -14,7 +41,7 @@ const SelectShelfModal = ({ options, addBook, show, setShow }) => {
     return (
         <Modal show={show} modalClosed={() => setShow(false)}>
             <div>
-                <select value={shelfId} onChange={handleChange}>
+                <Select value={shelfId} onChange={handleChange}>
                     {options.map((option) => {
                         return (
                             <option key={option?.id} value={option?.id}>
@@ -22,14 +49,14 @@ const SelectShelfModal = ({ options, addBook, show, setShow }) => {
                             </option>
                         );
                     })}
-                </select>
+                </Select>
                 <AddRemoveButton
                     color='green'
                     onClick={() => {
                         addBook(bookId, shelfId);
                         setShow(false);
                     }}>
-                    Ok
+                    Add
                 </AddRemoveButton>
             </div>
         </Modal>
