52b30f668b1fbb21fd301c19f6116c198bc894be
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-10-20 20:23:07 +1300

    add redux actions reducers constants for signin and store

---
+++ src/store.js
@@ -0,0 +1,26 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { signedInReducer } from './reducers/signedInReducers';
+
+const reducer = combineReducers({
+    signedIn: signedInReducer,
+});
+
+// const signedInFromStorage = localStorage.getItem('bookstoreSignedIn')
+//     ? JSON.parse(localStorage.getItem('bookstoreSignedIn'))
+//     : false;
+
+const initialState = {
+    signedIn: false,
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
