5e489420b696ccc9c75bf4170e083e3d75da36a4
Nathan Ballantyne <nathan.ballantyne@gmail.com>
2020-10-27 19:06:47 +1300

    assign to a variable before exporting

--- src/components/UI/ReactLinkStyle/Link.styled.js
+++ src/components/UI/ReactLinkStyle/Link.styled.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-
 const StyledLink = styled(Link)`
     text-decoration: none;
 
-    &:focus, &:hover, &:visited, &:link, &:active {
+    &:focus,
+    &:hover,
+    &:visited,
+    &:link,
+    &:active {
         text-decoration: none;
     }
-    margin-right: 10px; 
+    margin-right: 10px;
     margin-left: 5px;
     color: #000;
 `;
 
-export default (props) => <StyledLink {...props} />;
+const SLink = (props) => <StyledLink {...props} />;
+
+export default SLink;
